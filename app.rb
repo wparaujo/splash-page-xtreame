c4aac251ea2310731b5b3546a5c79c695e87ae4b
wsickert <wesley.sickert@gmail.com>
2016-11-23 18:10:23 -0200

    create theme home and do work email

--- app.rb
+++ app.rb
@@ -1,6 +1,16 @@
 require 'sinatra'
+require 'pony'
 
 get '/' do
-	erb :index
+	erb :home
+end
+
+post '/send-email' do
+	Pony.mail :to => params[:email],
+						:from => 'wesley.sickert@gmail.com',
+						:subject => 'Você agora é prioridade!',
+						:body => 'Obrigado!'
+
+	redirect to '/'
 end
 
